85d47fcd65ed42c70fe8f0b27c8d2e9885668097
Tural Novruzov <tural.nt.1@gmail.com>
2018-07-31 12:39:40 +0400

    Added comments to FeedReader class.

--- feedreader.py
+++ feedreader.py
@@ -3,11 +3,18 @@ import json
 
 
 class FeedReader:
+    """
+    Reads a given feed feed
+    """
     def __init__(self, feed='http://content.warframe.com/dynamic/worldState.php'):
         self.http = urllib3.PoolManager()
         self.feed = feed
 
     async def read(self):
+        """
+        Reads the feed
+        :return:
+        """
         res = self.http.request('GET', self.feed)
         if res.status != 200:
             raise Exception('Status not 200.')
