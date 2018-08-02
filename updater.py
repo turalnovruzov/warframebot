a3c6265ecadd277ca6c0d61ed2cf49d21840b68c
Tural Novruzov <tural.nt.1@gmail.com>
2018-08-02 21:34:21 +0400

    Fixed an issue.

--- updater.py
+++ updater.py
@@ -1,11 +1,13 @@
 from feedreader import FeedReader
 from notifier import Notifier
+from dbcontext import DbContext
 from discord.ext.commands import Bot
+import asyncio
 
 
 class Updater:
     def __init__(self):
         self.interval = 5
 
-    async def loop(self, bot: Bot, feedreader: FeedReader, notifier: Notifier):
-        pass
+    async def loop(self, loop, bot: Bot, feed_reader: FeedReader, notifier: Notifier, db_context: DbContext):
+        await db_context.read_user_ids()
