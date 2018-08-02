9b3d17df7c8b677990a1d8685ce5478e933a03c8
Tural Novruzov <tural.nt.1@gmail.com>
2018-08-02 06:04:55 +0400

    Added subscribe_channel command to the bot.

--- main.py
+++ main.py
@@ -28,11 +28,25 @@ async def on_ready():
 async def subscribe(ctx):
     user_ids = await db_context.read_user_ids()
     if ctx.message.author.id not in user_ids:
-        await db_context.save_user_ids(ctx.message.author.id)
-        await bot.say('{}, You are subscribed to the feed now!'.format(ctx.message.author.mention))
+        await asyncio.wait([db_context.save_user_ids(ctx.message.author.id),
+                     bot.say('{}, You are subscribed to the feed now!'.format(ctx.message.author.mention))])
     else:
         await bot.say('{}, You are already subscribed, to unsubscribe type **!unsubscribe**.'
                       .format(ctx.message.author.mention))
+
+
+@bot.command(pass_context=True)
+async def subscribe_channel(ctx):
+    if ctx.message.author.server_permissions.administrator:
+        channel_ids = await db_context.read_channel_ids()
+        if ctx.message.channel.id not in channel_ids:
+            await asyncio.wait([db_context.save_channel_ids(ctx.message.channel.id),
+                          bot.say('{}, This channel is subscribed to the feed now!'.format(ctx.message.author.mention))])
+        else:
+            await bot.say('{}, This channel is already subscribed, to unsubscribe type **!unsubscribe**.'
+                          .format(ctx.message.author.mention))
+
+
 
 with open('token.txt', 'r') as file:
     token = file.readline()
