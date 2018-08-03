35fb3612d8eab9ac0576c83dc2a2ec937a1592f1
Tural Novruzov <tural.nt.1@gmail.com>
2018-08-03 06:46:41 +0400

    Updated command to use check methods of DbContext class

--- main.py
+++ main.py
@@ -32,8 +32,7 @@ async def on_ready():
 
 @bot.command(pass_context=True)
 async def subscribe(ctx):
-    user_ids = await db_context.read_user_ids()
-    if ctx.message.author.id not in user_ids:
+    if not await db_context.user_exists(ctx.message.author.id):
         await asyncio.wait([db_context.save_user_ids(ctx.message.author.id),
                             bot.say('{}, You are subscribed to the feed now!'.format(ctx.message.author.mention))])
     else:
@@ -44,15 +43,16 @@ async def subscribe(ctx):
 @bot.command(pass_context=True)
 async def subscribe_channel(ctx):
     if ctx.message.author.server_permissions.administrator:
-        channel_ids = await db_context.read_channel_ids()
-        if ctx.message.channel.id not in channel_ids:
+        if not await db_context.channel_exists(ctx.message.channel.id):
             await asyncio.wait([db_context.save_channel_ids(ctx.message.channel.id),
-                          bot.say('{}, This channel is subscribed to the feed now!'.format(ctx.message.author.mention))])
+                                bot.say('{}, This channel is subscribed to the feed now!'
+                                        .format(ctx.message.author.mention))])
         else:
             await bot.say('{}, This channel is already subscribed, to unsubscribe type **!unsubscribe**.'
                           .format(ctx.message.author.mention))
     else:
-        await bot.say('{}, You do not have permission to do this.')
+        await bot.say('{}, You do not have permission to ue this command.'
+                      .format(ctx.message.author.mention))
 
 
 bot.run(token, loop=loop)
